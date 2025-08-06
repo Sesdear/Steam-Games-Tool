6e29293edd17e1b9ad387e562f2c8cd64b361af5
HLNikNiky <56186901+Sesdear@users.noreply.github.com>
2025-08-06 11:55:25 +0700

    1

---
+++ Main.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamGamesTool
+{
+    internal class Main
+    {
+        public void UnZip(string fileName, string extract_dir)
+        {
+            if (Directory.Exists(extract_dir) == false) { Directory.CreateDirectory(extract_dir); }
+
+            try
+            {
+                ZipFile.ExtractToDirectory(fileName, extract_dir);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "❌ Error");
+            }
+        }
+        public void DownloadManifest(string appid, CheckBox checkBox)
+        {
+            SteamAutoAdd steamAutoAdd = new SteamAutoAdd();
+            if (!CheckForExist(appid)) { return; }
+            string url = $"https://steamdatabase1.s3.eu-north-1.amazonaws.com/{appid}.zip";
+            if (checkBox.Checked)
+            {
+                string selectedPath = "./";
+                ManifestResponse(url, selectedPath, appid);
+                UnZip($"./{appid}.zip", $"./{appid}");
+                string game_path = $"./{appid}";
+
+                steamAutoAdd.StartAdd(game_path);
+                MessageBox.Show("✔ Ready", "Game Add");
+            }
+            else
+            {
+                FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+                folderDialog.Description = "Choose folder to save";
+                folderDialog.ShowNewFolderButton = true;
+
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderDialog.SelectedPath;
+                    ManifestResponse(url, selectedPath, appid);
+                }
+            }
+            
+            
+        }
+        public void ManifestResponse(string url, string path, string appid)
+        {
+            if (string.IsNullOrEmpty(url)) { return; }
+            string path_filename = $"{path}/{appid}.zip";
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(url, path_filename);
+                    MessageBox.Show("Manifest saved in path: " + path_filename, "✔ Ready");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "❌ Error");
+            }
+        }
+        public bool CheckForExist(string appid)
+        {
+            string url = $"https://steamdatabase1.s3.eu-north-1.amazonaws.com/{appid}.zip";
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "HEAD";
+
+                using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        return true ;
+                    }
+                    else
+                    {
+                        MessageBox.Show("AppId doesn't exist", "❌ Error");
+                        return false ;
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                if (ex.Response is HttpWebResponse errorResponse)
+                {
+                    MessageBox.Show($"{(int)errorResponse.StatusCode} {errorResponse.StatusDescription}", "❌ Error");
+                    return false ;
+                }
+                else
+                {
+                    MessageBox.Show(ex.Message, "❌ Error on connect");
+                    return false ;
+                }
+            }
+
+        }
+    }
+}
---
+++ SteamAutoAdd.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamGamesTool
+{
+    public class SteamAutoAdd
+    {
+        public static void MoveManifests(string manifest, string dir_name)
+        {
+            Console.WriteLine(Path.GetFileName(manifest));
+            File.Move(manifest, $"C:\\Program Files (x86)\\Steam\\config\\depotcache\\{Path.GetFileName(manifest)}");
+        }
+        public static void MoveLuas(string lua, string dir_name)
+        {
+            Console.WriteLine(Path.GetFileName(lua));
+            File.Move(lua, $"C:\\Program Files (x86)\\Steam\\config\\stplug-in\\{Path.GetFileName(lua)}");
+
+        }
+        public static void RestartSteam()
+        {
+            try
+            {
+                
+                var steamProcesses = Process.GetProcessesByName("steam");
+
+                if (steamProcesses.Length == 0)
+                {
+                    Console.WriteLine("Steam не запущен.");
+                }
+                else
+                {
+                    Console.WriteLine("Завершение Steam...");
+
+                    foreach (var proc in steamProcesses)
+                    {
+                        proc.Kill();
+                    }
+
+                    
+                    foreach (var proc in steamProcesses)
+                    {
+                        proc.WaitForExit();
+                    }
+
+                    Console.WriteLine("Steam завершён.");
+                }
+
+                
+                string steamPath = @"C:\Program Files (x86)\Steam\steam.exe";
+
+                if (!File.Exists(steamPath))
+                {
+                    Console.WriteLine("Не найден файл steam.exe по пути: " + steamPath);
+                    return;
+                }
+
+                Console.WriteLine("Запуск Steam...");
+                Process.Start(steamPath);
+                Console.WriteLine("Steam перезапущен.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ошибка: " + ex.Message);
+            }
+        }
+
+        public void StartAdd(string path_name)
+        {
+            
+            string[] files = Directory.GetFiles($"./{path_name}");
+            foreach (var file in files)
+            {
+                string extension = Path.GetExtension(file).ToLower();
+                if (extension == ".lua")
+                {
+                    Console.WriteLine($"{file}");
+                    MoveLuas(file, path_name);
+                }
+                else if (extension == ".manifest")
+                {
+                    Console.WriteLine($"{file}");
+                    MoveManifests(file, path_name);
+                }
+            }
+            RestartSteam();
+        }
+    }
+}
